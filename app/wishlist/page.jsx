9ca4318681f7a067590b6bb49c03119834a3259d
mohittsharmaa <mohitt.sharmaa7@gmail.com>
2023-08-21 12:14:09 +0530

    second test

--- app/wishlist/page.jsx
+++ app/wishlist/page.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import Wrapper from "../Components/Wrapper";
 
 const Wishlist = () => {
-  return <div>Wishlistttttttttttttttttttttt</div>;
+  return (
+    <Wrapper>
+      <h1 className="text-[28px] md:text-[34px] mb-5 font-semibold">
+        Your Wishlist
+      </h1>
+    </Wrapper>
+  );
 };
 
 export default Wishlist;
